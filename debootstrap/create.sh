ffd037e644797e21ce38086df9ed63e7c288a1ad
Murali Paluru <leodotcloud@gmail.com>
2018-03-22 11:48:51 -0700

    debootstrap script to create rootfs

---
+++ debootstrap/create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+TARGET_ROOTFS_DIR=/tmp/rootfs/ubuntu_1604
+mkdir -p ${TARGET_ROOTFS_DIR}
+
+INCLUDE_PKGS="vim,qemu-kvm,qemu-utils,bridge-utils,genisoimage,curl,net-tools"
+
+debootstrap \
+  --arch=amd64 \
+  --variant=minbase \
+  --include="${INCLUDE_PKGS}" \
+  xenial \
+  ${TARGET_ROOTFS_DIR} \
+  http://archive.ubuntu.com/ubuntu/
+
+rm -rf ${TARGET_ROOTFS_DIR}/var/cache/apt/archives/*.deb
+rm -rf ${TARGET_ROOTFS_DIR}/usr/share/man
+rm -rf ${TARGET_ROOTFS_DIR}/usr/share/locale
+
+tar cvzf ${TARGET_ROOTFS_DIR}.tar.gz -C ${TARGET_ROOTFS_DIR} .

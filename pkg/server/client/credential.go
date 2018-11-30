3fb6a41fa21eed10511159f00622cfcc89e8854f
James Oliver <joliver@rancher.com>
2018-11-29 22:56:53 -0700

    Client bugfix

--- pkg/server/client/credential.go
+++ pkg/server/client/credential.go
@@ -60,7 +60,8 @@ func (c *RancherVMClient) CredentialGet(name string) (*vmapi.Credential, error) 
 }
 
 func (c *RancherVMClient) CredentialDelete(name string) error {
-	if resp, err := c.delete("/v1/credential/" + name); err != nil {
+	resp, err := c.delete("/v1/credential/" + name)
+	if err != nil {
 		return err
 	} else if resp.StatusCode != http.StatusNoContent {
 		return fmt.Errorf("CredentialDelete failed: %v", resp.Status)
